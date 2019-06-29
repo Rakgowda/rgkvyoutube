862f4768a71fdbe0b9c50c466137d5e0acec7477
Rakgowda <rakshithgowdakv@gmail.com>
2019-06-29 14:05:00 +0530

    Add files via upload

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,19 +7,25 @@ class Navbar extends Component {
     render() { 
         return ( 
         
-          <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a className="navbar-brand text-danger" href="#"><i className="fa fa-youtube m-2" style={this.style}></i>RGKV TECH CHANNEL</a> 
-            <form className="form-inline my-2 my-lg-0 ml-auto">
+          <nav className="navbar navbar-expand-sm bg-light navbar-light fixed-top">
+           <a class="navbar-brand" href="#"><i className="fa fa-youtube m-2" style={this.style}></i>RGKV TECH CHANNEL</a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+        
+          <div className="collapse navbar-collapse" id="navbarTognavbarSupportedContentglerDemo01">
+        
+            <div class="navbar-nav ml-auto">
+            <form class="form-inline my-2 my-lg-0">
               <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
-            </form>
+              </form>
+            </div>
 
           </div>
+     
         </nav>
+  
       
       );
     }
