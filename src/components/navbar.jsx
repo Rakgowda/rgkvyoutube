e047b012124adc972ec12bc81ee5c437b0d78290
Rakgowda <rakshithgowdakv@gmail.com>
2019-06-30 11:34:02 +0530

    Add files via upload

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "../../node_modules/jquery/dist/jquery.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 
 class Navbar extends Component {
      style = {
@@ -7,26 +9,27 @@ class Navbar extends Component {
     render() { 
         return ( 
         
-          <nav className="navbar navbar-expand-sm bg-light navbar-light fixed-top">
+          
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
            <a class="navbar-brand" href="#"><i className="fa fa-youtube m-2" style={this.style}></i>RGKV TECH CHANNEL</a>
-           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-        
-          <div className="collapse navbar-collapse" id="navbarTognavbarSupportedContentglerDemo01">
-        
-            <div class="navbar-nav ml-auto">
-            <form class="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      {/* <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li> */}
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
               </form>
-            </div>
+  </div>
+</nav>
 
-          </div>
-     
-        </nav>
-  
-      
       );
     }
 }
